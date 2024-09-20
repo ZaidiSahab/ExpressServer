48fccf737bf1643e6441599df4617bc0d933d4a1
ZaidiSahab <1mustafazaidi@gmail.com>
2024-09-20 05:19:44 +0500

    Adding files of server

--- server.mjs
+++ server.mjs
@@ -1,1 +1,20 @@
 console.log("Server side javascript")
+
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log('Hello World!', new Date())
+    res.send('Hello World!' + new Date())
+})
+
+app.get('/profile', (req, res) => {
+    console.log('Hello World!', new Date())
+    res.send('Profile Page\n' + new Date())
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
