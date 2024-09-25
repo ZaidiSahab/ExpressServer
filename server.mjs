57e35f3e51c36e12a157cfccea8c7408c5becdba
ZaidiSahab <1mustafazaidi@gmail.com>
2024-09-25 22:28:06 +0500

    Adding files of server

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 console.log("Server side javascript")
 
 import express from 'express'
-const app = express() 
+const app = express()
 
 app.get('/', (req, res) => {
     console.log('This is Hello World!', new Date())
@@ -11,6 +11,19 @@ app.get('/', (req, res) => {
 app.get('/profile', (req, res) => {
     console.log('This is Profile Page!', new Date())
     res.send('Profile Page\n' + new Date())
+
+})
+
+app.get('/weather', (req, res) => {
+    console.log('This is Profile Page!', new Date())
+    res.send({
+        city: "Karachi",
+        humidity: 44,
+        tempInC: 30,
+        high: 32,
+        low: 14
+    }
+    )
 
 })
 
